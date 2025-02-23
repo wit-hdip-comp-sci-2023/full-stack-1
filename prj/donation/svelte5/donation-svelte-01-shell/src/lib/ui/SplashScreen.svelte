1eaa37e618fcbbad24c5ebde91b832017c5ef5af
Eamonn de Leastar <edeleastar@wit.ie>
2025-02-23 10:08:37 +0000

    svelte 5 updates

---
+++ prj/donation/svelte5/donation-svelte-01-shell/src/lib/ui/SplashScreen.svelte
@@ -0,0 +1,31 @@
+<section class="hero is-fullheight is-default is-bold">
+  <div class="hero-head"></div>
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <div class="columns is-vcentered">
+        <div class="column is-5">
+          <figure class="image">
+            <img class="object-fit" src="/homer2.png" alt="homer" />
+          </figure>
+        </div>
+        <div class="column is-6 is-offset-1">
+          <h1 class="title is-2 mb-5">
+            <span class="icon mr-5"><i class="fas fa-map-marker-alt"></i></span><span
+              class="icon mr-4"><i class="fas fa-vote-yea"></i></span
+            >
+            <p>Donation</p>
+          </h1>
+          <h2 class="subtitle is-4 mt-2">Help me run Springfield!</h2>
+          <p class="has-text-centered">
+            <a class="button is-medium is-success" href="/signup">
+              <span class="icon"><i class="fas fa-plus"></i></span><span>Sign Up</span>
+            </a>
+            <a class="button is-medium is-link" href="/login">
+              <span class="icon"><i class="fas fa-sign-in"></i></span><span>Login</span>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
